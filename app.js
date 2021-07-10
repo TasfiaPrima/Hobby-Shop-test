ccee710545ecb359fa6039a539bdd01909c0abd6
TasfiaPrima <55846643+TasfiaPrima@users.noreply.github.com>
2021-07-10 20:07:42 +0600

    connected to database

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+//Connect to Database:
+mongoose
+  .connect(process.env.MongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to Database!");
+  })
+  .catch((error) => {
+    console.log("Error");
+  });
 
 module.exports = app;
